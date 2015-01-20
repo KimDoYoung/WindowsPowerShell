64785652250668d8a28a1f1438e92f3924cad094
Kim Do Young <kdy987@gmail.com>
2015-01-20 11:52:16 +0900

    misc modify

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,12 @@
 #---------------------------------------------------
-# 환경변수
+# global variables
 #---------------------------------------------------
-
 $GLOBAL:psshome="C:\Users\Administrator\Documents\WindowsPowerShell"
 $GLOBAL:JAVA_HOME="C:\Program Files\Java\jdk1.8.0_11"
 $GLOBAL:MAVEN_HOME="C:\java_utils\apache-maven-3.2.5"
 
 #---------------------------------------------------
-# 환경변수
+# Path
 #---------------------------------------------------
 $env:path += ';c:\Windows'
 $env:path += ';c:\Windows\System32'
@@ -24,12 +23,9 @@ Set-Alias evernote "C:\Program Files (x86)\Evernote\Evernote\Evernote.exe"
 Set-Alias luna "C:\java_utils\eclipse\eclipse.exe"
 Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
 Set-Alias ll Get-ChildItem
-#--------------------------------------------------------------
-# 외부 ps1의 실행
-#--------------------------------------------------------------
-# 명령어들
+
 #------------------------------------------------------------
-# Prompt
+# Prompt and Command
 #------------------------------------------------------------
 . "$psshome\prompt.ps1" 
 . "$psshome\command.ps1"
